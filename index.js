9a9dbd91096857de0c645d1bab9457559f79850d
Kayla Volk <kaylavolk8@gmail.com>
2022-12-20 20:59:51 -0500

    commit tree fca949b19535795e25f819a1cad55a46df897e32

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((books) => renderBooks(books))
+  console.log(books)
 }
 
 function renderBooks(books) {
@@ -15,3 +18,19 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
+// url is the place we are directing the request to, second argument is an object full of key/value pairs pertaining to the request
+// by default, fetch is making a GET request
+// fetch is asynchronous (whatever comes after the asychnornous function will continue running until the asynch method is done)
+
+// // GET REQUEST: 
+// fetch(URL) // return a promise object
+// // .then() => after the fetch request is complete, and response is received
+// // abibymous callback function
+// .then(function(response){ // response here is a readable stream (string)
+//   return response.json() // take the response and jsonify it
+// }) // another promise object
+// .then(function(data){
+//   // now we can do something with the requested data
+// })
+
